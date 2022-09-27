d8203005a7c35d3652c2397c4c160e1cbfe3e541
Antonio Domanski <antoniodomanski27@gmail.com>
2022-09-27 19:31:53 -0300

    Criaçao Do Pacote Model com a entidade garcom, Criacao do Pacote com a classe controller

---
+++ src/main/java/com/myproject/apirestaurante/controller/GarcomController.java
@@ -0,0 +1,4 @@
+package com.myproject.apirestaurante.controller;
+
+public class GarcomController {
+}
---
+++ src/main/java/com/myproject/apirestaurante/model/Garcom.java
@@ -0,0 +1,7 @@
+package com.myproject.apirestaurante.model;
+
+public class Garcom {
+
+    private Long id;
+    private String name;
+}
